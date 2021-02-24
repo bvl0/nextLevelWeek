7094bcd29d2f8553529339637127faa6066f0529
icaro156 <icaro1561@outlook.com>
2021-02-23 21:43:17 -0300

    dia 2 na metade

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import "reflect-metadata";
+import "./database"
 
 const app = express();
 
 app.get("/users", (req, res) => {
-  return res.json({ message :"online"});
+  return res.json({ message :"online e meteno"});
 })
 
 app.listen(3333);
